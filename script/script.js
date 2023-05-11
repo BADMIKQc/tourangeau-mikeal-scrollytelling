5f2ff1c8b48bee281dd9e1dc9812bc0f447b0048
BADMIKQc <93718641+BADMIKQc@users.noreply.github.com>
2023-05-11 17:42:51 -0400

    2.3a

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+
+gsap.registerPlugin(DrawSVGPlugin);
+
 const fleche = document.querySelector('.fleche');
 const body = document.querySelector('body');
 const sprite= document.querySelector(".sprite");
@@ -48,59 +51,233 @@ const vague= document.querySelector(".vague");
 const bulle2= document.querySelector(".bulle2");
 const bulle3= document.querySelector(".bulle3");
 const bulle4= document.querySelector(".bulle4");
+const chapitrewidth = document.querySelector("#chapitre1").scrollWidth;
 
 
 
+gsap.timeline({ scrollTrigger: {
+
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: 'header',
+  
+  }})
+
+
+.fromTo('#dessin', { drawSVG:'0% 0%' },{duration: 30,drawSVG:'0% 100%', ease: "linear"})
+
+
+
+
+/* chapitre 1 */
+gsap.timeline({ scrollTrigger: {
+
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: '#chapitre1',
+  
+  }})
+
+
+.fromTo('.sprite', 
+{ x: "0%" }, 
+{duration: 30, x:"50%", ease: "linear"})
+
+  .fromTo('.montagne', 
+    {  x:'10%' }, 
+    {duration: 30,  x: '7%', ease: "linear",},
+      '<'   
+  )
+  .fromTo('.plaine', 
+  {  x:'0%' }, 
+  {duration: 30,  x: '-40%', ease: "linear",},
+    '<'   
+)
+.fromTo('.foret', 
+{  x:'5%' }, 
+{duration: 30,  x: '-10%', ease: "linear",},
+  '<'   
+)
+  
+  
+  
+  
+  
+      
+/* chapitre 3 */
+
+
+gsap.timeline({ scrollTrigger: {
+
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: '#chapitre3',
+  
+  }})
+
+  .fromTo('.lame', 
+  { y: "0%" }, 
+  {duration: 6, y:"10%", ease: "linear", },'<'  )
+  .fromTo('.socle', 
+  { y: "0%", x:'0%' }, 
+  {duration: 6, y:"160vh%",x:'-17.5vw', ease: "linear", },'<'  )
+
+  .fromTo('.soutien', 
+  { y: "0%",x:'0%' }, 
+  {duration: 6, y:"150vh%",x:'9.5vw', ease: "linear", },'<'  )
+
+
+
+
+/* chapitre 4 */
 gsap
-.timeline()
+.timeline({ scrollTrigger: {
+
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: '#chapitre4',
+
+  toggleActions: 'play pause resume pause',
+  
+  }})
+
+  .fromTo('.dude_medit_sprite', 
+  { y: "0%" }, 
+  {duration: 6, y:"20%", ease: "linear",repeat: 4, yoyo: true , }, )
 
 .fromTo('.arbre1', 
     { rotation: '-10deg' }, 
-    {duration: 1, rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-            
+    {duration: 3, rotation:"10deg", ease: "linear", yoyo: true, repeat:8,},
+    '<'      
   )
   .fromTo('.arbre2', 
     { rotation: '-10deg' }, 
-    {duration: 1, rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-    "-=1.3"     
+    {duration: 3, rotation:"10deg", ease: "linear", yoyo: true, repeat:8,},
+    '<'    
   )
   .fromTo('.arbre3', 
     { rotation: '-10deg' }, 
-    {duration: 1, rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-    "-=1.3"
-  );
+    {duration: 3, rotation:"10deg", ease: "linear", yoyo: true, repeat:8,},
+    '<'
+  )
 
-  gsap
+ 
   .fromTo('.soleil', 
   { rotation: '0deg' }, 
-  {duration: 3, rotation:"360deg", ease: "linear",repeat: -1,},
+  {duration: 6, rotation:"360deg", ease: "linear",repeat:3}, '<'
           
-)
+);
 
+
+/* chapitre 5 */
 gsap
 .fromTo('.vague', 
 { x: "0%",y:"0%" }, 
-{duration: 30, x:"50%",y:"50%", ease: "linear",repeat: -1,},
+{duration: 30, x:"50%",y:"50%", ease: "linear",repeat: 2,scrollTrigger: {
+
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: '#chapitre5',
+
+  
+  
+  }}),
         
-)
 
 
+/* chapitre 6 */
 gsap
-.timeline()
+gsap.timeline({ scrollTrigger: {
 
+    scrub: true,
+  
+    pin: true,
+  
+    markers: true,
+  
+    start: 'top top',
+  
+    end: '300% bottom',
+  
+    trigger: '#chapitre6',
+  
+  toggleActions: 'restart complete reversereset',
+  }})
+
+  
 .fromTo('.bulle2', 
 { x:"-10%",rotation: '-10deg' }, 
-{duration: 3,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-"-=1.3"
+{duration: 3,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: 4,},
+"<"
             
   )
   .fromTo('.bulle3', 
   { x:"-10%",rotation: '-10deg' }, 
-  {duration: 1,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-  "-=1.3"     
+  {duration: 3,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: 4,},
+  "<"     
   )
   .fromTo('.bulle4', 
     { x:"-10%",rotation: '-10deg' }, 
-    {duration: 3,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: -1,},
-    "-=1.3"
-  );
+    {duration: 3,x:"10%", rotation:"10deg", ease: "linear", yoyo: true,repeat: 4,},
+    "<"
+  ).to('.perso_tue', 
+  { motionPath:'#ligne_drop',align:"#ligne_drop",alignOrigin:[0.5,0.5],duration:30, }, 
+ 
+  "<"     
+  )
+  .fromTo('.fantome_gauche', 
+  { x:"0%"}, 
+  {duration: 3,x:"-20%", ease: "linear"}, 
+ 
+  '-=20'     
+  )
+
+  .fromTo('.fantome_droite', 
+  { x:"0%"}, 
+  {duration: 3,x:"20%", ease: "linear"}, 
+ 
+  '<'     
+  )
+  
+  
+  ;
